1325715167d73be641930da357fb1afdccbff301
liguoping <liguoping01@58ganji.com>
2018-05-11 15:13:28 +0800

    上传图片接口

--- src/main/java/com/gshy/web/api/controllers/FileController.java
+++ src/main/java/com/gshy/web/api/controllers/FileController.java
@@ -1,13 +1,40 @@
 package com.gshy.web.api.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+
 import com.bj58.wf.mvc.ActionResult;
 import com.bj58.wf.mvc.annotation.Path;
+import com.bj58.ycs.tool.webutil.actionresult.ActionResult4JSON;
+import com.bj58.ycs.tool.webutil.tools.ParamHelper;
+import com.gshy.web.service.utils.FileUtils;
 
 @Path("/file")
-public class FileController {
+public class FileController extends BaseController{
 
 	@Path("/upload")
 	public ActionResult upload(){
-		return null;
+		try {
+			String accessToken = ParamHelper.getString(beat, "accessToken", "");
+			String mediaId = ParamHelper.getString(beat, "mediaId", "");
+			if (StringUtils.isBlank(accessToken) || StringUtils.isBlank(mediaId)) {
+				log.info("accessToken: " + accessToken + ", " + accessToken);
+				return new ActionResult4JSON("{\"ret\":\"-1\",\"msg\":\"参数错误!\"}");
+			}
+			
+			String dateFile = new SimpleDateFormat("yyyyMMdd").format(new Date());
+			String picPath = "/opt/web/static.haoyejinfu.com/"+dateFile+"/";
+			String picName = UUID.randomUUID().toString().replace("-", "") + ".jpg";
+			FileUtils.saveImageToDisk(accessToken, mediaId, picName, picPath);
+			String picUrl = "http://static.haoyejinfu.com/"+dateFile+picName;
+			log.info(picUrl);
+			return new ActionResult4JSON("{\"ret\":\"1\",\"msg\":\"success!\",\"data\":\""+picUrl+"\"}");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ActionResult4JSON("{\"ret\":\"-1\",\"msg\":\"上传失败!\"}");
 	}
 }
