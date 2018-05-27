e53bd9f52f7be090e16a09a07251c2bbd4628022
liguoping <liguoping01@58ganji.com>
2018-05-27 12:05:55 +0800

    add

--- src/main/java/com/bj58/web/api/controllers/FileController.java
+++ src/main/java/com/bj58/web/api/controllers/FileController.java
@@ -6,10 +6,14 @@ import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.alibaba.fastjson.JSONObject;
+import com.bj58.web.api.utils.APIConfig;
 import com.bj58.wf.mvc.ActionResult;
 import com.bj58.wf.mvc.annotation.Path;
 import com.bj58.ycs.tool.webutil.actionresult.ActionResult4JSON;
 import com.bj58.ycs.tool.webutil.tools.ParamHelper;
+import com.darengong.tools.util.net.http.HttpClient;
+import com.darengong.tools.util.net.http.HttpResponse;
 import com.gshy.web.service.utils.FileUtils;
 
 @Path("/file")
@@ -19,11 +23,21 @@ public class FileController extends BaseController{
 	
 	@Path("/accesstoken")
 	public ActionResult accessToken(){
-		if(StringUtils.isNotBlank(accesstoken)){
-			return new ActionResult4JSON("{\"ret\":\"1\",\"data\":\""+accesstoken+"\"}"); 
-		}else{
-			return new ActionResult4JSON("{\"ret\":\"1\",\"data\":\"error\"}"); 
+		try {
+			String accesstoken = "";
+			String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + APIConfig.appid
+					+ "&secret=" + APIConfig.appsecret;
+			HttpResponse response = HttpClient.getInstance().get(url);
+			String content = response.getContent();
+			JSONObject jsonObject = JSONObject.parseObject(content);
+			accesstoken = jsonObject.getString("access_token");
+			if (StringUtils.isNotBlank(accesstoken)) {
+				return new ActionResult4JSON("{\"ret\":\"1\",\"data\":\"" + accesstoken + "\"}");
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return new ActionResult4JSON("{\"ret\":\"-1\",\"msg\":\"error!\"}");
 	}
 	
 
